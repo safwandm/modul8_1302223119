37ba25516c2f78b080e77bc5d649be733ce9cee3
safwandm <safwan.damanik@gmail.com>
2024-04-20 09:01:33 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302223119/BankTransferConfig.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_1302223119
+{
+    class Config
+    {
+        public string lang {  get; set; }
+        public Transfer transfer { get; set; }
+        public string[] methods { get; set; }
+
+        public Confirmation confirmation { get; set; }
+
+        public Config(string lang, Transfer transfer, string[] methods, Confirmation confirmation = null)
+        {
+            this.lang = lang;
+            this.transfer = transfer;
+            this.methods = methods;
+            this.confirmation = confirmation;
+        }
+    }
+
+    class Transfer
+    {
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+
+        public Transfer(int threshold, int low_fee, int high_fee)
+        {
+            this.threshold = threshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+        }
+    }
+
+    class Confirmation
+    {
+        public string en {  get; set; }
+        public string id { get; set; }
+
+        public Confirmation(string en, string id)
+        {
+            this.en = en;
+            this.id = id;
+        }
+    }
+
+    internal class BankTransferConfig
+    {
+        public Config config;
+        private string json;
+
+        public BankTransferConfig(string path)
+        {
+
+            try
+            {
+                config = ReadConfigFile(path);
+
+            } catch (Exception ex)
+            {
+                SetDefault();
+                WriteNewConfigFile(path);
+            }
+        }
+        public Config ReadConfigFile(string path)
+        {
+            return JsonSerializer.Deserialize<Config>(File.ReadAllText(path));
+        }
+        public void WriteNewConfigFile(string path)
+        {
+            JsonSerializerOptions option = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            json = JsonSerializer.Serialize(config, option);
+            File.WriteAllText(path, json);
+        }
+        public void SetDefault()
+        {
+            config = new Config("en", new Transfer(25000000, 6500, 15000), ["RTO(real - time)", "SKN", "RTGS", "BI FAST"], new Confirmation("yes", "ya"));
+        }
+
+    }
+}
--- modul8_1302223119/Program.cs
+++ modul8_1302223119/Program.cs
@@ -1,2 +1,80 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul8_1302223119;
+using System.Collections;
+
+BankTransferConfig btc = new BankTransferConfig(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\bank_transfer_config.json");
+
+Console.WriteLine(btc.config.lang);
+
+Config cf = btc.config;
+
+if  (btc.config.lang == "en")
+{
+    Console.WriteLine("Please insert the amount of money to transfer:");
+} else if (cf.lang == "id")
+{
+    Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+}
+
+long money = Convert.ToInt64(Console.ReadLine());
+
+if (money <= cf.transfer.threshold)
+{
+    money += cf.transfer.low_fee;
+    Console.WriteLine($"Transfer fee = {cf.transfer.low_fee} \nTotal amount = {money}");
+
+
+}
+else if (money > cf.transfer.threshold)
+{
+    money += cf.transfer.high_fee;
+    Console.WriteLine($"Transfer fee = {cf.transfer.high_fee} \nTotal amount = {money}");
+
+}
+
+if (cf.lang == "en")
+{
+    Console.WriteLine("Select transfer method: ");
+}  else if (cf.lang == "id")
+{
+    Console.WriteLine("Pilih metode transfer: ");
+}
+
+ int metode = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+foreach (string method in cf.methods)
+{
+    Console.WriteLine($"{i} {method}");
+    ++i;
+}
+
+
+if (cf.lang == "en")
+{
+    Console.WriteLine($"Please type \"{cf.confirmation.en}\" to confirm the transaction: ");
+}
+else if (cf.lang == "id")
+{
+    Console.WriteLine($"Ketik \"{cf.confirmation.id}\" untuk mengkonfirmasi transaksi:");
+}
+
+Console.ReadLine();
+
+
+if (metode > 4 || metode <= 0)
+{
+    
+}
+else
+{
+    if (cf.lang == "en")
+    {
+        Console.WriteLine($"The transfer is completed");
+    }
+    else if (cf.lang == "id")
+    {
+        Console.WriteLine($"Proses transfer berhasil");
+    }
+}
+
